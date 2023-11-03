13f5aaedc400327358c5a1ad0379222fbc6d8d6e
lorgik <sib.lorgik@gmail.com>
2023-11-03 17:17:34 +0300

    fix

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -3,7 +3,14 @@ const menu = document.querySelector(".menu");
 const body = document.querySelector("body");
 const menuLinks = document.querySelectorAll(".menu__navigation-link");
 
+const toggleBtnTitle = toggleBtn?.querySelector(".blot-btn__title");
+
 toggleBtn?.addEventListener("click", function () {
+  if (menu?.classList.contains("is-open")) {
+    toggleBtnTitle.textContent = "Меню";
+  } else {
+    toggleBtnTitle.textContent = "Закрыть";
+  }
   menu?.classList.toggle("is-open");
   body?.classList.toggle("scroll-lock");
 });
@@ -11,6 +18,7 @@ toggleBtn?.addEventListener("click", function () {
 function resetMenu() {
   menu?.classList.remove("is-open");
   body?.classList.remove("scroll-lock");
+  toggleBtnTitle.textContent = "Меню";
 }
 
 menuLinks.forEach((link) =>
